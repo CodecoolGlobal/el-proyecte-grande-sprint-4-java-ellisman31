7ee46eae6ffdbf5266f7eefdb683d8bdc7311cc9
ellisman31 <akos99@protonmail.com>
2021-10-25 15:14:55 +0200

    setup(#28,#29,#30): Spring boot project initialized

---
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/ForcedepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.forcedepartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForcedepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForcedepartmentApplication.class, args);
+	}
+
+}
