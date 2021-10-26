bf2e595f5784caf6e4a682319802790b5a01f1b9
ellisman31 <akos99@protonmail.com>
2021-10-27 00:00:38 +0200

    feat(#41): make a dummy list for save and get all the information after registration

--- forcedepartment/src/main/java/com/codecool/forcedepartment/service/UserService.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/service/UserService.java
@@ -1,4 +1,28 @@
 package com.codecool.forcedepartment.service;
 
+import com.codecool.forcedepartment.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
+
+    //Dummy, it will be with DAO
+
+    private List<User> test = new ArrayList<>();
+
+    public void addUserTest(User user) {
+        test.add(user);
+    }
+
+    public void getAllInformation() {
+        for (User user: test) {
+            System.out.println("First name: " + user.getFirstName());
+            System.out.println("Last name: " +user.getLastName());
+            System.out.println("Email address: " +user.getEmail());
+            System.out.println("Date of birth: " +user.getBirthOfDate());
+            System.out.println("Password: " +user.getPassword());
+            System.out.println("User type: " +user.getUserType());
+        }
+    }
 }
