79ede2aec5b38d0fd8bf15f2a864b3fbf3d264e4
ellisman31 <akos99@protonmail.com>
2021-10-26 13:19:39 +0200

    setup(#35): base user model to other user types

--- forcedepartment/src/main/java/com/codecool/forcedepartment/model/User.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/model/User.java
@@ -2,4 +2,19 @@ package com.codecool.forcedepartment.model;
 
 public abstract class User {
 
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String phone_number;
+    private boolean isAdmin;
+    private String groupName;
+
+    public User(String firstName, String lastName, int age, String phone_number, boolean isAdmin, String groupName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.phone_number = phone_number;
+        this.isAdmin = isAdmin;
+        this.groupName = groupName;
+    }
 }
