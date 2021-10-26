ab6112a9fbd635cb170a59805c75ce3666b75220
ellisman31 <akos99@protonmail.com>
2021-10-26 13:21:08 +0200

    setup(#37): worker user model

--- forcedepartment/src/main/java/com/codecool/forcedepartment/model/Worker.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/model/Worker.java
@@ -1,4 +1,12 @@
 package com.codecool.forcedepartment.model;
 
 public class Worker extends User {
+
+    private static final boolean IS_ADMIN = false;
+    private String description;
+    private double rate;
+
+    public Worker(String firstName, String lastName, int age, String phone_number, String groupName) {
+        super(firstName, lastName, age, phone_number, IS_ADMIN, groupName);
+    }
 }
