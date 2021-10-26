fdebe29368aad5825cfc4dadcaeed07554293102
ellisman31 <akos99@protonmail.com>
2021-10-26 13:20:19 +0200

    setup(#36): regular user model

--- forcedepartment/src/main/java/com/codecool/forcedepartment/model/RegularUser.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/model/RegularUser.java
@@ -1,4 +1,11 @@
 package com.codecool.forcedepartment.model;
 
 public class RegularUser extends User {
+
+    private static final boolean IS_ADMIN = false;
+
+    public RegularUser(String firstName, String lastName, int age, String phone_number, String groupName) {
+        super(firstName, lastName, age, phone_number, IS_ADMIN, groupName);
+    }
+
 }
