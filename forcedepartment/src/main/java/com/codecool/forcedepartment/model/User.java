9f14afd0371e1444315ce2c3ee92dfee6a327113
ellisman31 <akos99@protonmail.com>
2021-10-26 15:02:21 +0200

    fix(#35): remove constructor and change groupName to groupType as an enum because of readability

--- forcedepartment/src/main/java/com/codecool/forcedepartment/model/User.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/model/User.java
@@ -1,5 +1,7 @@
 package com.codecool.forcedepartment.model;
 
+import com.codecool.forcedepartment.model.util.GroupTypes;
+
 public abstract class User {
 
     private String firstName;
@@ -7,14 +9,54 @@ public abstract class User {
     private int age;
     private String phone_number;
     private boolean isAdmin;
-    private String groupName;
+    private GroupTypes groupType;
 
-    public User(String firstName, String lastName, int age, String phone_number, boolean isAdmin, String groupName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.phone_number = phone_number;
-        this.isAdmin = isAdmin;
-        this.groupName = groupName;
+    public GroupTypes getGroupType() {
+        return groupType;
     }
+
+    public void setGroupType(GroupTypes groupType) {
+        this.groupType = groupType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
 }
