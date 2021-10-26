2b560b9327bf239b7725366abbec0294602e60e6
ellisman31 <akos99@protonmail.com>
2021-10-26 15:01:07 +0200

    fix(#36, #37): remove constructor

--- forcedepartment/src/main/java/com/codecool/forcedepartment/model/Admin.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/model/Admin.java
@@ -4,8 +4,5 @@ public class Admin extends User {
 
     private static final boolean IS_ADMIN = true;
 
-    public Admin(String firstName, String lastName, int age, String phone_number, String groupName) {
-        super(firstName, lastName, age, phone_number, IS_ADMIN, groupName);
-    }
 
 }
--- forcedepartment/src/main/java/com/codecool/forcedepartment/model/RegularUser.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/model/RegularUser.java
@@ -4,8 +4,4 @@ public class RegularUser extends User {
 
     private static final boolean IS_ADMIN = false;
 
-    public RegularUser(String firstName, String lastName, int age, String phone_number, String groupName) {
-        super(firstName, lastName, age, phone_number, IS_ADMIN, groupName);
-    }
-
 }
