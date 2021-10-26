3324f6730549e9622c7a25e211e127a6a4014042
ellisman31 <akos99@protonmail.com>
2021-10-26 13:20:46 +0200

    setup(#38): admin user model

--- forcedepartment/src/main/java/com/codecool/forcedepartment/model/Admin.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/model/Admin.java
@@ -1,4 +1,11 @@
 package com.codecool.forcedepartment.model;
 
 public class Admin extends User {
+
+    private static final boolean IS_ADMIN = true;
+
+    public Admin(String firstName, String lastName, int age, String phone_number, String groupName) {
+        super(firstName, lastName, age, phone_number, IS_ADMIN, groupName);
+    }
+
 }
