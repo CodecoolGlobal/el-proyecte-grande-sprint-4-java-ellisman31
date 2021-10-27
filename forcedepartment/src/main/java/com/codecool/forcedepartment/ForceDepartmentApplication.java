d9d1efd65f4e273a4b3441b3fbe7aff1ff05f7a2
dorogimate <dorogimate6@gmail.com>
2021-10-27 14:49:52 +0200

    refactor(#25): refactor WorkerDaoJdbc

--- forcedepartment/src/main/java/com/codecool/forcedepartment/ForceDepartmentApplication.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/ForceDepartmentApplication.java
@@ -27,8 +27,8 @@ public class ForceDepartmentApplication implements CommandLineRunner {
 		//System.out.println(databaseManager.getWorkersByRating());
 		//System.out.println(databaseManager.getWorkersByProfession("Heating and Cooling System Mechanic"));
 		//System.out.println(databaseManager.getWorkersByWorkObject("Terrace"));
+		//System.out.println(databaseManager.getAllWorkObject());
 		//System.out.println(databaseManager.getWorkersByNamePart("ha")); // had to refactor for lower case - does not work for Haynes
 		//System.out.println(databaseManager.getAllProfession());
-		//System.out.println(databaseManager.getAllWorkObject());
 	}
 }
