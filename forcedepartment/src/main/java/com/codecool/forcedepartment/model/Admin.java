d9aca3545a93a258958a8ef4763696d947949684
ellisman31 <akos99@protonmail.com>
2021-10-27 10:47:21 +0200

    feat(#38): add super to constructor

--- forcedepartment/src/main/java/com/codecool/forcedepartment/model/Admin.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/model/Admin.java
@@ -1,8 +1,12 @@
 package com.codecool.forcedepartment.model;
 
+import com.codecool.forcedepartment.model.util.UserTypes;
+
 public class Admin extends User {
 
     private static final boolean IS_ADMIN = true;
 
-
+    public Admin(String firstName, String lastName, String registrationDate, String birthOfDate, UserTypes userType, String password, String email) {
+        super(firstName, lastName, registrationDate, birthOfDate, IS_ADMIN, userType, password, email);
+    }
 }
