f438e90269c1a07ac57804debc4f2c6b21b9004f
dorogimate <dorogimate6@gmail.com>
2021-10-27 15:01:01 +0200

    refactor(#25): add methods to UserDao

--- forcedepartment/src/main/java/com/codecool/forcedepartment/dao/UserDao.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/dao/UserDao.java
@@ -1,19 +1,37 @@
 package com.codecool.forcedepartment.dao;
 
+import com.codecool.forcedepartment.model.User;
+
+import java.util.List;
+
 public interface UserDao {
 
     // add new regular user
 
+    void addNewRegularUser();
+
     // add new worker
+
+    void addNewWorker();
 
     // get all data about user
 
+    User getDataAboutUser(int id);
+
+    List<User> getAllDataAboutUser();
+
     // check if user exist - email
+
+    boolean checkIfUserExists(String email);
 
     // login check if user exist - email + password
 
+    boolean checkIfValidLogin(String email, String password);
+
     // update
 
+    void editRegularProfile(String firstName, String lastName, String birthOfDate, String email);
 
+    void editWorkerProfile(String firstName, String lastName, String birthOfDate, String email, String description, String phoneNumber, List<String> profession);
 
 }
