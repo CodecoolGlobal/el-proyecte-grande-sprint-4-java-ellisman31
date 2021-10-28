55e02124890dcd56fc0367aa971891223132dd76
ellisman31 <akos99@protonmail.com>
2021-10-28 10:46:21 +0200

    chor(#48): remove profession list and use worker list instead

--- forcedepartment/src/main/java/com/codecool/forcedepartment/model/Profession.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/model/Profession.java
@@ -7,7 +7,6 @@ import java.util.Map;
 public class Profession {
 
     //private Map<String, Double> professions = new HashMap<>();
-    private List<String> professions = new ArrayList<>();
     private String professionName;
     private double experience_year;
 
@@ -24,14 +23,6 @@ public class Profession {
 
     public void setExperience_year(double experience_year) {
         this.experience_year = experience_year;
-    }
-
-    public void addProfession(String profession) {
-        professions.add(profession);
-    }
-
-    public List<String> getProfessions() {
-        return professions;
     }
 
     public String getProfessionName() {
