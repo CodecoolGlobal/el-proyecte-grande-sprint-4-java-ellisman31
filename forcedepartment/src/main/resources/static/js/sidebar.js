907a227c56c2e08757d501755eb1eefe87f019c9
dorogimate <dorogimate6@gmail.com>
2021-10-28 14:09:12 +0200

    feature(#2): add open and close feature to sidebar

--- forcedepartment/src/main/resources/static/js/sidebar.js
+++ forcedepartment/src/main/resources/static/js/sidebar.js
@@ -1,9 +1,26 @@
 const container = document.getElementById('worker-container');
 const sidebar = document.getElementById('sidebar');
 
+const sidebarProfession = document.getElementById('profession-search');
 
 function myTimer() {
     sidebar.style.height = `${container.scrollHeight-200}px`
 }
 
 myTimer();
+
+sidebarProfession.addEventListener('click', (e) => {
+    e.preventDefault();
+    let node = document.createElement("LI");
+    let textnode = document.createTextNode("Water");
+    node.appendChild(textnode);
+    node.style.fontSize = '18px'
+    node.style.marginLeft = '20px';
+    if (sidebarProfession.childNodes.length < 2) {
+        sidebarProfession.appendChild(node)
+    } else {
+
+        sidebarProfession.removeChild(sidebarProfession.childNodes[1]);
+    }
+
+})
