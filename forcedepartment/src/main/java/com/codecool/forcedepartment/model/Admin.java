470da17a67e348bcc4d2f2810118cdf24dbaaca3
ellisman31 <akos99@protonmail.com>
2021-10-28 10:17:20 +0200

    chor(#38): remove unnecessary fields from super

--- forcedepartment/src/main/java/com/codecool/forcedepartment/model/Admin.java
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/model/Admin.java
@@ -4,9 +4,7 @@ import com.codecool.forcedepartment.model.util.UserTypes;
 
 public class Admin extends User {
 
-    private static final boolean IS_ADMIN = true;
-
     public Admin(String firstName, String lastName, String registrationDate, String birthOfDate, String userType, String email) {
-        super(firstName, lastName, registrationDate, birthOfDate, IS_ADMIN, userType, email);
+        super(firstName, lastName, registrationDate, birthOfDate, userType, email);
     }
 }
