a905995229c48420446d157f705485313af85658
dorogimate <dorogimate6@gmail.com>
2021-10-28 14:13:38 +0200

    feature(#2): add api route that lists all professions

---
+++ forcedepartment/src/main/java/com/codecool/forcedepartment/controller/api/GetAllProfessionApi.java
@@ -0,0 +1,28 @@
+package com.codecool.forcedepartment.controller.api;
+
+import com.codecool.forcedepartment.dao.DatabaseManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class GetAllProfessionApi {
+
+    DatabaseManager databaseManager;
+
+    @Autowired
+    public GetAllProfessionApi(DatabaseManager databaseManager) {
+        this.databaseManager = databaseManager;
+    }
+
+    @RequestMapping(value = "/api/getAllProfession", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody List<String> getAllProfession() {
+        return databaseManager.getAllProfession();
+    }
+
+}
