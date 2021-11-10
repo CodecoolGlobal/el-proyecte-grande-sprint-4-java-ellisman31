3fdd46533dfee09f58fab3ec1ed23f622c3ab96f
ellisman31 <akos99@protonmail.com>
2021-11-10 12:56:14 +0100

    feat(#2-1): login route create in app

--- frontend/forcedepartment-app/src/App.js
+++ frontend/forcedepartment-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Main from './components/Main';
 import About from './components/About';
 import Register from './components/register/Register'
+import Login from './components/login/Login'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<Main/>}/>
                 <Route path="/about_us" element={<About/>}/>
                 <Route path="/registration" element={<Register/>}/>
+                <Route path="/login" element={<Login/>}/>
             </Routes>
         </Router>
     );
