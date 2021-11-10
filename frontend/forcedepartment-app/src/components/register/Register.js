ef158523e9b786ac7a42ba9d93f30d940314c35a
ellisman31 <akos99@protonmail.com>
2021-11-10 11:23:21 +0100

    feat(#3-2): separate the registration panels and save registration data from any user

--- frontend/forcedepartment-app/src/components/register/Register.js
+++ frontend/forcedepartment-app/src/components/register/Register.js
@@ -1,35 +1,29 @@
 import '../css/Register.css';
 import {useEffect, useState} from "react";
+import  {useNavigate } from 'react-router';
+import RegisterForEveryUser from "./RegisterForEveryUser";
+import RegisterForWorker from "./RegisterForWorker";
 
 //TODO: sent data to the database, get data from database
-function Register(props) {
+function Register() {
 
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [birthOfDate, setBirthOfDate] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordAgain, setPasswordAgain] = useState('');
+    const userTypeData = ["USER", "WORKER"];
     const [userType, setUserType] = useState('');
-    const userTypesTemp = ["USER", "WORKER"];
-
-    const [errorMessage, setErrorMessage] = useState('');
-    const [getData, setGetData] = useState();
-
-    const [sendData, setSendData] = useState({
-        firstName: firstName, lastName: lastName, email: email, birthOfDate: birthOfDate, password: password,
-        userType: userType
-    })
+    const [previousData, setPreviousData]= useState([]);
+    const [workerData, setWorkerData] = useState([]);
+    const current = new Date();
+    const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()} ${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}:${current.getMilliseconds()}`;
 
     useEffect(() => {
         document.title = "Registration";
-        setErrorMessage('');
     }, []);
+
+    const navigate = useNavigate();
 
     const saveDataIntoTheDatabase = async () => {
         const response = await fetch('', {
             method: "POST",
-            body: JSON.stringify(sendData)
+            body: JSON.stringify(previousData)//sendData
         })
     }
     //in the useEffect
@@ -38,77 +32,32 @@ function Register(props) {
         )
             .then((res) => res.json())
             .then((res) => {
-                setGetData(res);
+                //setGetData(res);
             })
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        errorMessageSetter();
+    const userTypeHandler = (user) => {
+        setUserType(user)
     }
 
-    const errorMessageSetter = () => {
-        if (password !== passwordAgain) {
-            setErrorMessage("The passwords do not match!\n Try again!")
-        } //set to email
-        else if (password !== passwordAgain && email === email) {
-            setErrorMessage("The passwords do not match and the email address is already in use!\n Try again!")
-        }
-        else {
-            setErrorMessage('');
-        }
+    const previousDataHandler = (data) => {
+        setPreviousData(data);
     }
+
+    const workerDataHandler = (data) => {
+        setWorkerData(data);
+    }
+
+    console.log(previousData);
+    console.log(date);
 
     return (
         <div className="container">
             <div className="register-form">
-                <br/>
-                <h1>Registration</h1>
-                {{errorMessage} ?
-                    <p id="wrongLogin">{errorMessage}</p> : null}
-                <hr/>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" id="firstName" name="firstName" placeholder="First Name"
-                           value={firstName}
-                           onChange={(e) => setFirstName(e.target.value)}
-                           required/>
-                    <input type="text" id="lastName" name="lastName" placeholder="Last Name"
-                           value={lastName}
-                           onChange={(e) => setLastName(e.target.value)}
-                           required/>
-                    <input type="email" id="email" name="email" placeholder="Email address"
-                           value={email}
-                           onChange={(e) => setEmail(e.target.value)}
-                           required/>
-                    <input type="date" id="userBirthOfDate" name="userBirthOfDate" placeholder="Birth of date"
-                           value={birthOfDate}
-                           onChange={(e) => setBirthOfDate(e.target.value)}
-                           required/>
-                    <div className="password">
-                        <input type="password" id="password" name="password" placeholder="Password"
-                               value={password}
-                               onChange={(e) => setPassword(e.target.value)}
-                               required/>
-                        <input type="password" id="passwordAgain" name="passwordAgain" placeholder="Password again"
-                               value={passwordAgain}
-                               onChange={(e) => setPasswordAgain(e.target.value)}
-                               required/>
-                    </div>
-                    {userTypesTemp.map((user) =>
-                        <span className="userType">
-                            <input type="radio" value={user} id="radio" name="groupType"
-                                   onChange={(e) => setUserType(e.target.value)}
-                                   required/> {user}
-                        </span>
-                    )}
-                    <button type="submit" value="Submit">Submit</button>
-                </form>
-
-                <div className="mainPageHref">
-                    <a href="/">
-                        <button type="button" value="Main page">Main page</button>
-                    </a>
-                </div>
+                {userType === "WORKER" ? <RegisterForWorker previousData={previousData}
+                                                            workerDataHandler={workerDataHandler} navigate={navigate}/> :
+                    <RegisterForEveryUser userTypeData={userTypeData} previousDataHandler={previousDataHandler}
+                                          userTypeHandler={userTypeHandler} navigate={navigate}/>}
             </div>
         </div>
     )
