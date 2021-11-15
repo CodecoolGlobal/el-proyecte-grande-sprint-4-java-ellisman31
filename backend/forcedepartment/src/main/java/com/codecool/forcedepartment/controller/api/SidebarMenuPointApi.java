1e6710b0a22f186e925356f0771f00b12b1aab2d
dorogimate <dorogimate6@gmail.com>
2021-11-15 16:43:16 +0100

    refactor (#14 - 2): separate api routes into classes

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/controller/api/SidebarMenuPointApi.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/controller/api/SidebarMenuPointApi.java
@@ -1,7 +1,6 @@
 package com.codecool.forcedepartment.controller.api;
 
 import com.codecool.forcedepartment.dao.DatabaseManager;
-import com.codecool.forcedepartment.model.User;
 import com.codecool.forcedepartment.model.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -10,27 +9,17 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @Controller
 @CrossOrigin(origins = "http://localhost:3000")
-public class GetAllProfessionApi {
+public class SidebarMenuPointApi {
 
     DatabaseManager databaseManager;
 
     @Autowired
-    public GetAllProfessionApi(DatabaseManager databaseManager) {
+    public SidebarMenuPointApi(DatabaseManager databaseManager) {
         this.databaseManager = databaseManager;
     }
 
-    @RequestMapping(value = "/api/getAllProfession", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody List<String> getAllProfession() {
-        return databaseManager.getAllProfession();
-    }
-
-    @RequestMapping(value = "/api/getAllWorkObject", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody List<String> getAllWorkObject() {
-        return databaseManager.getAllWorkObject();
-    }
 
     @RequestMapping(value = "/api/getAllWorkerByProfession/{profession}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<Worker> getAllWorkerByProfession(
@@ -38,23 +27,13 @@ public class GetAllProfessionApi {
         return databaseManager.getWorkersByProfession(profession);
     }
 
+
     @RequestMapping(value = "/api/getAllWorkerByWorkObject/{workObject}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<Worker> getAllWorkerByWorkObject(
             @PathVariable("workObject") String workObject) {
         return databaseManager.getWorkersByWorkObject(workObject);
     }
 
-    @RequestMapping(value = "/api/getWorkersByRating", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody List<Worker> getAllWorkersByRating() {
-        return databaseManager.getWorkersByRating();
-    }
-
-
-    @RequestMapping(value = "/api/getUserById/{userId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody
-    User getWorkerById(
-            @PathVariable("userId") int userId
-    ) { return databaseManager.getDataAboutUser(userId);}
 
     @RequestMapping(value = "/api/getWorkerByExtraSearch/{name}/{workObject}/{profession}/{rating}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
