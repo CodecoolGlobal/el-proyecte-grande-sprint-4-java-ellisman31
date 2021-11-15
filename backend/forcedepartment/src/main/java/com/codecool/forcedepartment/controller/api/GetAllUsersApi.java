dbae3f078e6b2ae4758932a64183e6d54a8af5cd
dorogimate <dorogimate6@gmail.com>
2021-11-15 21:38:31 +0100

    refactor (#14 - 3): handled error when user or worker is registering

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/controller/api/GetAllUsersApi.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/controller/api/GetAllUsersApi.java
@@ -4,9 +4,11 @@ import com.codecool.forcedepartment.dao.DatabaseManager;
 import com.codecool.forcedepartment.model.util.GetLatestId;
 import com.codecool.forcedepartment.model.User;
 import com.codecool.forcedepartment.model.Worker;
+import com.google.gson.Gson;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,13 +18,14 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 @CrossOrigin(origins = "http://localhost:3000")
 public class GetAllUsersApi {
 
     DatabaseManager databaseManager;
-    private static int workerId;
+    private int workerId;
 
     @Autowired
     public GetAllUsersApi(DatabaseManager databaseManager) {
@@ -35,9 +38,8 @@ public class GetAllUsersApi {
         return databaseManager.getAllDataAboutUser();
     }
 
-    @RequestMapping(value = "/api/getAllUser", method = RequestMethod.POST)
-    public void addRegularUser(@RequestBody String userJson) throws JSONException, ParseException {
-
+    @ResponseBody @RequestMapping(value = "/api/getAllUser", method = RequestMethod.POST)
+    public String addRegularUser(@RequestBody String userJson) throws JSONException, ParseException {
         JSONObject user = new JSONObject(userJson);
 
         String firstName = user.getString("firstName");
@@ -46,14 +48,12 @@ public class GetAllUsersApi {
         String userType = user.getString("userType");
         String password = user.getString("password");
         String email = user.getString("email");
-
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         Date birthOfDateParsed = formatter.parse(birthOfDate);
 
         User newUser = new User(firstName, lastName, birthOfDateParsed, userType, email);
-
         workerId = databaseManager.registerRegularUser(newUser, password);
-
+        return "User created";
     }
 
     @RequestMapping(value = "/api/getAllWorker", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -62,14 +62,15 @@ public class GetAllUsersApi {
         return databaseManager.getWorkersByRating();
     }
 
-    @RequestMapping(value = "/api/getAllWorker", method = RequestMethod.POST)
-    public void addWorker(@RequestBody String workerJson) throws JSONException {
+    @ResponseBody @RequestMapping(value = "/api/getAllWorker", method = RequestMethod.POST)
+    public String addWorker(@RequestBody String workerJson) throws JSONException {
         JSONObject worker = new JSONObject(workerJson);
-
+        
         String description = worker.getString("description");
         String phoneNumber = worker.getString("telephoneNumber");
 
-        databaseManager.registerWorker(workerId, phoneNumber, description);
+        databaseManager.registerWorker(databaseManager.getLatestId("website_user") + 1, phoneNumber, description);
+        return "Worker created";
     }
 
 }
