bf73a1da89d4321d94b1a82c40f2845c7a447916
dorogimate <dorogimate6@gmail.com>
2021-11-23 12:59:24 +0100

    feat (#1): refactor database connection

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/controller/api/MainPageApi.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/controller/api/MainPageApi.java
@@ -1,7 +1,11 @@
 package com.codecool.forcedepartment.controller.api;
 
 import com.codecool.forcedepartment.dao.DatabaseManager;
+import com.codecool.forcedepartment.model.Profession;
 import com.codecool.forcedepartment.model.Worker;
+import com.codecool.forcedepartment.service.ProfessionService;
+import com.codecool.forcedepartment.service.WorkObjectService;
+import com.codecool.forcedepartment.service.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -14,26 +18,33 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 public class MainPageApi {
 
-    DatabaseManager databaseManager;
+    private ProfessionService professionService;
+    private WorkObjectService workObjectService;
+    private WorkerService workerService;
 
     @Autowired
-    public MainPageApi(DatabaseManager databaseManager) {
-        this.databaseManager = databaseManager;
+    public MainPageApi(ProfessionService professionService,
+                       WorkObjectService workObjectService,
+                       WorkerService workerService) {
+        this.professionService = professionService;
+        this.workObjectService = workObjectService;
+        this.workerService = workerService;
     }
+
 
     @RequestMapping(value = "/api/getAllProfession", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<String> getAllProfession() {
-        return databaseManager.getAllProfession();
+        return professionService.getAllProfession();
     }
 
     @RequestMapping(value = "/api/getAllWorkObject", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<String> getAllWorkObject() {
-        return databaseManager.getAllWorkObject();
+        return workObjectService.getAllWorkObjects();
     }
 
     @RequestMapping(value = "/api/getWorkersByRating", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<Worker> getAllWorkersByRating() {
-        return databaseManager.getWorkersByRating();
+        return workerService.getAllWorkersByRate();
     }
 
 }
--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/model/User.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/model/User.java
@@ -1,150 +1,43 @@
 package com.codecool.forcedepartment.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Set;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
 
-    private int userId;
+    @Id
+    private Long id;
+
     private String firstName;
     private String lastName;
-    private int age;
-    private Date registrationDate;
-    private Date birthOfDate;
-    private boolean isAdmin;
-    private String userType;
+    private Date registration_date;
+    private Date birth_date;
+    private boolean is_admin;
     private String password;
     private String email;
+    private String group_name;
     private String image = "profile-icon-empty.png";
-    private String imageName;
-    private static final boolean IS_ADMIN = false;
+    //private String imageName;
+    //private static final boolean IS_ADMIN = false;
     //private String profileImage;
 
-    public User() {
-    }
-
-    public User(String firstName, String lastName, Date birthOfDate, String userType, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthOfDate = birthOfDate;
-        this.userType = userType;
-        this.email = email;
-    }
-
-    public User(int id, String firstName, String lastName, Date registrationDate, Date birthOfDate, String userType, String email) {
-        this.userId = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.registrationDate = registrationDate;
-        this.birthOfDate = birthOfDate;
-        this.userType = userType;
-        this.email = email;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public String getImageName() {
-        return firstName + " " + lastName;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public int getAge() {
-
-        Calendar actualDate = Calendar.getInstance();
-        actualDate.setTime(getRegistrationDate());
-
-        Calendar birthOfDate = Calendar.getInstance();
-        birthOfDate.setTime(getBirthOfDate());
-
-        return actualDate.get(Calendar.YEAR) - birthOfDate.get(Calendar.YEAR);
-    }
-
-
-    public void setUserType(String userType) {
-        this.userType = userType;
-    }
-
-    public String getUserType() {
-        return userType;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        //do regex to check the email
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public boolean isAdmin() {
-        return isAdmin;
-    }
-
-    public void setAdmin(boolean admin) {
-        isAdmin = admin;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                ", registrationDate='" + registrationDate + '\'' +
-                ", birthOfDate='" + birthOfDate + '\'' +
-                ", isAdmin=" + isAdmin +
-                ", userType=" + userType +
-                ", email='" + email + '\'' +
-                "}\n";
-    }
-
-    public Date getRegistrationDate() {
-        return registrationDate;
-    }
-
-    public void setRegistrationDate(Date registrationDate) {
-        this.registrationDate = registrationDate;
-    }
-
-    public Date getBirthOfDate() {
-        return birthOfDate;
-    }
-
-    public void setBirthOfDate(Date birthOfDate) {
-        this.birthOfDate = birthOfDate;
-    }
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private Set<Worker> workerSet;
 }
