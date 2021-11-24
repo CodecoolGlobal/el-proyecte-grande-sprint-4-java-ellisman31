e79c2370071cf769042961c22403df0217bb11bb
ellisman31 <akos99@protonmail.com>
2021-11-24 14:29:54 +0100

    feat(#1): create validation queries for registration and for login

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/dal/UserRepository.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/dal/UserRepository.java
@@ -3,6 +3,7 @@ package com.codecool.forcedepartment.dal;
 import com.codecool.forcedepartment.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -13,4 +14,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(value = "SELECT MAX(u.id) from User u")
     Long getLatestId();
+
+    @Query(value = "SELECT u.id\n FROM User u WHERE u.email =:email")
+    Long isEmailAlreadyInExist(@Param("email") String email);
+
+    @Query(value = "SELECT u.id\n FROM User u WHERE u.email =:email AND u.password =:password")
+    Long isUserInExist(@Param("email") String email, @Param("password") String password);
 }
