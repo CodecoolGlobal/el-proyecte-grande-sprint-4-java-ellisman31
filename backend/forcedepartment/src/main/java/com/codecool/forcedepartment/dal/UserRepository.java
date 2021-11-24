9677cd1e64950fdfa560e2bce0d87143a6ed9e53
ellisman31 <akos99@protonmail.com>
2021-11-24 09:31:41 +0100

    feat(#1): get the latest id from the database for worker

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/dal/UserRepository.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/dal/UserRepository.java
@@ -2,6 +2,15 @@ package com.codecool.forcedepartment.dal;
 
 import com.codecool.forcedepartment.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "SELECT MAX(u.id) from User u")
+    Long getLatestId();
 }
