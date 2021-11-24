d5eb565b30295a1cbe4b4abf27c3a1d1a8a95dd7
ellisman31 <akos99@protonmail.com>
2021-11-24 14:28:54 +0100

    feat(#3): get all profession for workers and change id generatedvalue type

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/model/User.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/model/User.java
@@ -1,7 +1,7 @@
 package com.codecool.forcedepartment.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +10,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 @Setter
@@ -18,15 +19,10 @@ import java.util.Set;
 @NoArgsConstructor
 @Entity
 @Table(name = "website_user", schema = "public")
-@JsonIgnoreProperties(value = {"id"})
 public class User {
 
     @Id
-    @SequenceGenerator(
-            name = "website_user_id_seq",
-            sequenceName = "website_user_id_seq",
-            allocationSize = 1
-    )
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
     private String first_name;
@@ -38,15 +34,23 @@ public class User {
     private LocalDateTime registration_date;
     private String group_name;
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JsonBackReference
-    private Set<Worker> worker;
-
     private static final boolean IS_ADMIN = false;
     //private String image = "profile-icon-empty.png";
     //private String imageName;
     //private String profileImage;
 
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonBackReference
+    private Set<Worker> worker;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "workerExperience",
+            joinColumns = @JoinColumn(name = "worker_id", foreignKey = @ForeignKey(name = "fk_worker_id", value = ConstraintMode.NO_CONSTRAINT)),
+            inverseJoinColumns = @JoinColumn(name = "id")
+    )
+    @JsonManagedReference
+    private List<WorkerExperience> workerExperience;
 
     public User(String first_name, String last_name, Date birth_date, String email, String password, String group_name) {
         this.first_name = first_name;
