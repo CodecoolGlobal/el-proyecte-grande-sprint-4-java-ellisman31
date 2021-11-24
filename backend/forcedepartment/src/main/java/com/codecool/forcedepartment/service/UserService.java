7b4fbe2cc3e02061be45e5239da864c938c2d50e
ellisman31 <akos99@protonmail.com>
2021-11-24 09:33:30 +0100

    feat(#9): add getLatestId method to the service

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/service/UserService.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/service/UserService.java
@@ -5,7 +5,6 @@ import com.codecool.forcedepartment.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -34,5 +33,8 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
+    public Long getTheLatestId() {
+        return userRepository.getLatestId() + 1;
+    }
 
 }
