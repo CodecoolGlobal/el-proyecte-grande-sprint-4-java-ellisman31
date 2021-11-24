69db06ba2bf48885f60a500b9a5eb78694914b26
ellisman31 <akos99@protonmail.com>
2021-11-24 14:31:59 +0100

    feat(#10): connection with profession to get information and remove unused jsonignore

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/model/WorkerExperience.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/model/WorkerExperience.java
@@ -1,34 +1,39 @@
 package com.codecool.forcedepartment.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
+import java.util.*;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@JsonIgnoreProperties(value = {"id","worker_id","profession_id"})
 public class WorkerExperience {
 
     @Id
-    @JsonIgnore
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
-    @JsonIgnore
     private Long worker_id;
-
     private Long profession_id;
     private double experience_years;
 
     @ManyToMany(mappedBy = "workerExperience",  cascade = { CascadeType.ALL }, fetch = FetchType.EAGER)
     @JsonBackReference
-    private List<Worker> workers;
+    private List<User> user;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "profession_id", referencedColumnName = "id", insertable = false, updatable = false)
+    @JsonManagedReference
+    private Profession profession;
 
 }
