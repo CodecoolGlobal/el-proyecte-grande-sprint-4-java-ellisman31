4c52dc7e409b5fc2b4cb21d7851c45289a1e831b
dorogimate <dorogimate6@gmail.com>
2021-11-25 10:04:13 +0100

    refactor (#7): refactor profile that worker data displayed correctly

--- frontend/forcedepartment-app/src/components/profile/Profile.js
+++ frontend/forcedepartment-app/src/components/profile/Profile.js
@@ -9,43 +9,27 @@ const Profile = () => {
 
     const { userId } = useParams(0);
     const [currentWorker, setCurrentWorker] = useState([]);
-    const [userData, setUserData] = useState([]);
-    const currentProfessions = currentWorker.workerExperience;
+    const [currentUser, setCurrentUser] = useState([]);
+    const [currentProfessions, setCurrentProfessions] = useState([]);
 
     useEffect(()=> {
         document.title = "Special Department |  "+ userId;
     },[userId]);
 
-/*     useEffect(() => {
-        const getWorker = async () => {
-            const worker = await fetchSingleWorker();
-            setCurrentWorker(worker);
-        }
-        getWorker();
-    })
-
-    const fetchSingleWorker = async() => {
-        const response = await fetch(`http://localhost:8080/api/getUserById/${userId}`)
-        const data = await response.json();
-        return data;
-    } */
-
     useEffect(() => {
-        const getWorker = async () => {
-            const response = await fetch(`http://localhost:8080/api/getUserById/${userId}`);
-            const data = await response.json();
-            setUserData(data.user);
-            setCurrentWorker(data);
+            const getWorker = async () => {
+                const response = await fetch(`http://localhost:8080/api/getWorkerById/${userId}`);
+                const data = await response.json();
+                console.log(data);
+                setCurrentProfessions(data.user.workerExperience);
+                setCurrentUser(data.user);
+                setCurrentWorker(data);
         }
-        getWorker();
-        
+            getWorker();
         }, [userId]);
 
-    const fetchWorkerById = async () => {
 
-    }
-
-    //const workerAge = Math.floor(Math.floor(Math.abs(new Date - Date.parse(currentWorker.user.birth_date))/ (24*60*60*1000)) / 365);
+    const workerAge = Math.floor(Math.floor(Math.abs(new Date - Date.parse(currentUser.birth_date))/ (24*60*60*1000)) / 365);
 
     
 
@@ -56,7 +40,7 @@ const Profile = () => {
             <div className="user-container">
                 <div className="user-personal">
                     <img src={profilePic} alt={currentWorker.imageName}></img>
-                    <h1>{`${userData.first_name} ${userData.last_name}`}</h1>
+                    <h1>{`${currentUser.first_name} ${currentUser.last_name} ( ${workerAge} )`}</h1>
                     <h2>Phone number: </h2>
                     <p>{currentWorker.phone_number}</p>
                 </div>
@@ -67,10 +51,10 @@ const Profile = () => {
                     <p className="user-description-content">{currentWorker.description}</p>
                     <h2 id="h2-profession">Profession(s): </h2>
                     <span className="professions">
-                        {/* {currentProfessions ? currentProfessions.map((profession, i, arr) => 
-                        <span key={profession}>
-                            {arr.length - 1 === i ? profession : `${profession}, `}
-                        </span>) : ''} */}
+                        {currentProfessions ? currentProfessions.map((profession, i, arr) => 
+                        <span key={profession.experience_years}>
+                            {arr.length - 1 === i ? profession.profession.profession_name : `${profession.profession.profession_name}, `}
+                        </span>) : ''}
                     </span>
                 </div>
             </div>
