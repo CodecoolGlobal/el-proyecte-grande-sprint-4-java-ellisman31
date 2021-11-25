3408ca41bb3e9b31dc9a4431ed6de007b4a6cfb0
ellisman31 <akos99@protonmail.com>
2021-11-25 11:44:50 +0100

    feat(#6): connection with work requirement

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/model/WorkObject.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/model/WorkObject.java
@@ -1,20 +1,24 @@
 package com.codecool.forcedepartment.model;
 
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@JsonIgnoreProperties(value = {"id"})
+@JsonIgnoreProperties(value = {"id", "workRequirement"})
 public class WorkObject {
 
     @Id
@@ -22,4 +26,14 @@ public class WorkObject {
     private Long id;
 
     private String work_object;
+
+    @ManyToMany()
+    @JoinTable(
+            name = "workRequirement",
+            joinColumns = @JoinColumn(name = "work_object_id", foreignKey = @ForeignKey(name = "fk_work_object_id",
+                    value = ConstraintMode.NO_CONSTRAINT)),
+            inverseJoinColumns = @JoinColumn(name = "id")
+    )
+    @JsonManagedReference
+    private List<WorkRequirement> workRequirement;
 }
