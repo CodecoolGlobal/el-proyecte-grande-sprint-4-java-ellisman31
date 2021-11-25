ab9268471704d5ec407095f5134424dc88d5485b
ellisman31 <akos99@protonmail.com>
2021-11-25 11:44:21 +0100

    feat(#4): connection with work requirement

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/model/Profession.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/model/Profession.java
@@ -2,9 +2,11 @@ package com.codecool.forcedepartment.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -13,7 +15,7 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@JsonIgnoreProperties(value = {"id"})
+@JsonIgnoreProperties(value = {"id", "workRequirement"})
 public class Profession {
 
     @Id
@@ -25,4 +27,15 @@ public class Profession {
     @OneToMany(mappedBy = "profession", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JsonBackReference
     private Set<WorkerExperience> workerExperience;
+
+    @ManyToMany()
+    @JoinTable(
+            name = "workRequirement",
+            joinColumns = @JoinColumn(name = "profession_id", foreignKey = @ForeignKey(name = "fk_profession_id",
+                    value = ConstraintMode.NO_CONSTRAINT)),
+            inverseJoinColumns = @JoinColumn(name = "id")
+    )
+    @JsonManagedReference
+    private List<WorkRequirement> workRequirement;
+
 }
