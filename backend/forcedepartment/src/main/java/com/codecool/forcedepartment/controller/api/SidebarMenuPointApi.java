7bd4945bb711a10c11fdca3f20f4ee9374b39da1
ellisman31 <akos99@protonmail.com>
2021-11-25 11:48:38 +0100

    feat(#12): API calls get the right data from service

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/controller/api/SidebarMenuPointApi.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/controller/api/SidebarMenuPointApi.java
@@ -1,6 +1,7 @@
 package com.codecool.forcedepartment.controller.api;
 
 import com.codecool.forcedepartment.model.Worker;
+import com.codecool.forcedepartment.service.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -13,32 +14,37 @@ import java.util.List;
 public class SidebarMenuPointApi {
 
 
-    //@Autowired
+    private WorkerService workerService;
 
+    @Autowired
+    public SidebarMenuPointApi(WorkerService workerService) {
+        this.workerService = workerService;
+    }
 
     @RequestMapping(value = "/api/getAllWorkerByProfession/{profession}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<Worker> getAllWorkerByProfession(
             @PathVariable("profession") String profession) {
-        return null; //databaseManager.getWorkersByProfession(profession);
+        return workerService.getAllWorkersByProfession(profession);
     }
 
 
     @RequestMapping(value = "/api/getAllWorkerByWorkObject/{workObject}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<Worker> getAllWorkerByWorkObject(
             @PathVariable("workObject") String workObject) {
-        return null;//databaseManager.getWorkersByWorkObject(workObject);
+        return workerService.getAllWorkersByWorkObject(workObject);
     }
 
 
-    @RequestMapping(value = "/api/getWorkerByExtraSearch/{name}/{workObject}/{profession}/{rating}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/api/getWorkerByExtraSearch/{name}/{workObject}/{profession}/{rating}",
+            method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     List<Worker> getWorkersByExtraFilter(
             @PathVariable("name") String name,
             @PathVariable("workObject") String workObject,
             @PathVariable("profession") String profession,
-            @PathVariable("rating") int rating
+            @PathVariable("rating") double rating
     ) {
-        return null;//databaseManager.getAllByFilter(name, workObject, profession, rating);
+        return workerService.getAllWorkersByExtraFilter(name, workObject, profession, rating);
     }
 
 }
