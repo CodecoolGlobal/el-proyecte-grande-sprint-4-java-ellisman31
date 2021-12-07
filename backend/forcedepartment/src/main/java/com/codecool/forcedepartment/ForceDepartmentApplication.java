d105e9bc37a86ff6b63090fa228eecc35e70af12
ellisman31 <akos99@protonmail.com>
2021-12-08 00:21:09 +0100

    refact(#1): remove password encoder from application

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/ForceDepartmentApplication.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/ForceDepartmentApplication.java
@@ -2,12 +2,8 @@ package com.codecool.forcedepartment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.sql.SQLException;
-
 
 @SpringBootApplication
 public class ForceDepartmentApplication {
@@ -15,8 +11,4 @@ public class ForceDepartmentApplication {
 		SpringApplication.run(ForceDepartmentApplication.class, args);
 	}
 
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 }
