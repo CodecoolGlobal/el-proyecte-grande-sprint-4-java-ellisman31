77c0c0f851d671641caa989d642fb8462676966d
ellisman31 <akos99@protonmail.com>
2021-12-07 09:26:39 +0100

    feat(#12-1): add filter to the configuration and hide specific and set role to routes

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/security/SecurityConfig.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/security/SecurityConfig.java
@@ -11,7 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -29,13 +31,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        //customAuthenticationFilter.setFilterProcessesUrl("/new_login_path"); //custom login and pass this to the filter
         http
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(STATELESS)
                 .and()
-                .authorizeRequests().anyRequest().permitAll()
+                .authorizeRequests().antMatchers("/api/token/refresh", "/", "/register", "/login").permitAll()
                 .and()
-                .addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+                .authorizeRequests().antMatchers(GET, "/api/**").hasAnyAuthority("ADMIN")
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .addFilter(new CustomAuthenticationFilter(authenticationManagerBean()))
+                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
