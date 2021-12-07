e5400a6dc73a34bd6f7ec619a32df40d09da3144
ellisman31 <akos99@protonmail.com>
2021-12-08 00:22:28 +0100

    feat(#1-3): replace unsafe information with JWT config properties and set permissions for specific routes

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/security/SecurityConfig.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/security/SecurityConfig.java
@@ -1,55 +1,77 @@
 package com.codecool.forcedepartment.security;
 
-import lombok.RequiredArgsConstructor;
+import com.codecool.forcedepartment.security.Filters.CustomAuthenticationFilter;
+import com.codecool.forcedepartment.security.Filters.CustomAuthorizationFilter;
+import com.codecool.forcedepartment.security.JWT.JWTConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.http.HttpMethod.GET;
+import javax.crypto.SecretKey;
+
+import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
     private final UserDetailsService userDetailsService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final SecretKey secretKey;
+    private final JWTConfig jwtConfig;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder,
+                          UserDetailsService userDetailsService, SecretKey secretKey, JWTConfig jwtConfig) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
+        this.secretKey = secretKey;
+        this.jwtConfig = jwtConfig;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean(), secretKey, jwtConfig);
         //customAuthenticationFilter.setFilterProcessesUrl("/new_login_path"); //custom login and pass this to the filter
         http
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(STATELESS)
                 .and()
-                .authorizeRequests().antMatchers("/api/token/refresh", "/", "/register", "/login").permitAll()
+                .authorizeRequests().antMatchers("/", "/register", "/login").permitAll()
                 .and()
                 .authorizeRequests().antMatchers(GET, "/api/**").hasAnyAuthority("ADMIN")
                 .and()
+                .authorizeRequests().antMatchers(POST, "/api/**").hasAnyAuthority("ADMIN")
+                .and()
+                .authorizeRequests().antMatchers(PUT, "/api/**").hasAnyAuthority("ADMIN")
+                .and()
+                .authorizeRequests().antMatchers(DELETE, "/api/**").hasAnyAuthority("ADMIN")
+                .and()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
-                .addFilter(new CustomAuthenticationFilter(authenticationManagerBean()))
-                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .addFilter(new CustomAuthenticationFilter(authenticationManagerBean(), secretKey, jwtConfig))
+                .addFilterBefore(new CustomAuthorizationFilter(secretKey, jwtConfig), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
-    @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 }
