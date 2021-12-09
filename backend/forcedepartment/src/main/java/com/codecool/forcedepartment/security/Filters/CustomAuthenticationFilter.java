efa08f964daa279746dbe3493ab2656a224041fe
ellisman31 <akos99@protonmail.com>
2021-12-09 17:31:27 +0100

    feat(#1): add personal information about user to the token claim after successful authentication

--- backend/forcedepartment/src/main/java/com/codecool/forcedepartment/security/Filters/CustomAuthenticationFilter.java
+++ backend/forcedepartment/src/main/java/com/codecool/forcedepartment/security/Filters/CustomAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.codecool.forcedepartment.security.Filters;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.codecool.forcedepartment.security.JWT.JWTConfig;
+import com.codecool.forcedepartment.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,8 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -29,12 +29,14 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     private final AuthenticationManager authenticationManager;
     private final SecretKey secretKey;
     private final JWTConfig jwtConfig;
+    private final UserService userService;
     private final static int PLUS_DAY_AFTER_ACCESS_TOKEN = 14;
 
-    public CustomAuthenticationFilter(AuthenticationManager authenticationManager, SecretKey secretKey, JWTConfig jwtConfig) {
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager, SecretKey secretKey, JWTConfig jwtConfig, UserService userService) {
         this.authenticationManager = authenticationManager;
         this.secretKey = secretKey;
         this.jwtConfig = jwtConfig;
+        this.userService = userService;
     }
 
     @Override
@@ -59,6 +61,13 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         User user = (User) authResult.getPrincipal();
         Algorithm algorithm = Algorithm.HMAC256(String.valueOf(secretKey).getBytes());
 
+        com.codecool.forcedepartment.model.User currentUser = userService.getUserByEmail(user.getUsername()).get();
+        Map<String, String> getPersonalInformation = new HashMap<>();
+        getPersonalInformation.put("firstName", currentUser.getFirst_name());
+        getPersonalInformation.put("lastName", currentUser.getLast_name());
+        getPersonalInformation.put("emailAddress", currentUser.getEmail());
+        getPersonalInformation.put("userId", String.valueOf(currentUser.getId()));
+
         String accessToken = JWT.create()
                 .withSubject(user.getUsername())
                 .withExpiresAt(java.sql.Date.valueOf(LocalDate.now().plusDays(jwtConfig.getTokenExpirationAfterDays())))
@@ -66,6 +75,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 .withClaim("roles",
                         user.getAuthorities().stream()
                                 .map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .withClaim("userPersonalInformation", getPersonalInformation)
                 .sign(algorithm);
 
         String refreshToken = JWT.create()
