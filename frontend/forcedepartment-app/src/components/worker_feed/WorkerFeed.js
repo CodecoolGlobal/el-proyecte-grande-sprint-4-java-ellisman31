2502757ff22579470d6de017dd54ec5a0f33c0b4
ellisman31 <akos99@protonmail.com>
2021-12-09 02:06:39 +0100

    feat(#18): get the proper data from fetch for display workers

--- frontend/forcedepartment-app/src/components/worker_feed/WorkerFeed.js
+++ frontend/forcedepartment-app/src/components/worker_feed/WorkerFeed.js
@@ -1,17 +1,17 @@
 import './Header.css';
 import WorkerCard from "./worker_card/WorkerCard";
 
-
 const WorkerFeed = (props) => {
+
     return (
         <div>
-            {props.workers.length === 0 ? 
-                <div > <p id="no-worker-found">No worker found!</p> </div> : 
+            {props.workers.data?.length === 0 ?
+                <div > <p id="no-worker-found">No worker found!</p> </div> :
                 <div id="worker-card-container">
-                    {props.workers.map((worker) => (
-                    <WorkerCard key={worker.user.id} worker={worker} />))} 
+                    {props.workers.data?.map((worker) => (
+                    <WorkerCard key={worker.user.id} worker={worker} />))}
                 </div>
-            }    
+            }
         </div>
 
     )
